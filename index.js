c8943b5d0cbdda8e13e0c51af81259c98557d52b
hosobenzdent <hosobenz@gmail.com>
2026-04-28 21:38:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ body {
   background-size: 100% 100%; 
   background-repeat: no-repeat;
   position: relative;
-  padding: 300px 80px 80px 80px;
+  padding: 260px 60px 80px 60px;
   box-sizing: border-box; 
 }
 
